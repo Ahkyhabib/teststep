b206a109bdb6fffbb5197dd3f576465a344dcb1a
Akhirah Strong <akhirahstrong@quickenloans.com>
2018-12-21 10:49:04 -0500

    Change TextField to a custom TextBox

--- src/components/Panel1.js
+++ src/components/Panel1.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import TextField from '@material-ui/core/TextField';
+import TextBox from './TextBox';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+    
+    
+  },
+};
 
 
 export class Panel1 extends Component {
   render() {
+// Puling in values (fistName, lastName...)
+const { values, handleChange } = this.props;
+
     return (
-      <div>
-        <Header />
-        <Footer />
+     
         
-      </div>
+        <MuiThemeProvider>
+        <React.Fragment>
+        <Header />
+          
+          <br />
+          <TextBox />
+          
+          <Footer />
+        </React.Fragment>
+      </MuiThemeProvider>
+       
+        
+    
     )
   }
 }
---
+++ src/components/TextBox.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  dense: {
+    marginTop: 16,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+const currencies = [
+  {
+    value: 'USD',
+    label: '$',
+  },
+  {
+    value: 'EUR',
+    label: '€',
+  },
+  {
+    value: 'BTC',
+    label: '฿',
+  },
+  {
+    value: 'JPY',
+    label: '¥',
+  },
+];
+
+class OutlinedTextFields extends React.Component {
+  state = {
+    name: '',
+    age: '',
+    multiline: 'Controlled',
+    currency: 'EUR',
+    address:''
+  };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          id="outlined-name"
+          label="Name"
+          className={classes.textField}
+          value={this.state.name}
+          onChange={this.handleChange('name')}
+          margin="normal"
+          variant="outlined"
+        />
+        <br/>
+        <TextField
+          id="outlined-address"
+          label="Address"
+          className={classes.textField}
+          value={this.state.address}
+          onChange={this.handleChange('address')}
+          margin="normal"
+          variant="outlined"
+        />
+        
+      </form>
+    );
+  }
+}
+
+OutlinedTextFields.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(OutlinedTextFields);
